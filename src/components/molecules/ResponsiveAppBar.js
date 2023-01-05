4aa78d2757bac447cb8d26f1a2eb7e91033f8b85
Eugenia Pastore <eugenia.pastore@gmail.com>
2023-01-05 10:15:24 +0100

    fixed failed build and deploy

--- src/components/molecules/ResponsiveAppBar.js
+++ src/components/molecules/ResponsiveAppBar.js
@@ -21,21 +21,13 @@ const pages = [
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   return (
